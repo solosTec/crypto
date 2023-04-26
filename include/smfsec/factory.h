7c0495e42ce5facf619f9bbfbb7fbc956aadeb16
Sylko Olzscher <sylko.olzscher@gmail.com>
2023-04-26 10:04:23 +0200

    fix: missing reference to boost added

--- include/smfsec/factory.h
+++ include/smfsec/factory.h
@@ -8,125 +8,145 @@
 #ifndef CYNG_CRYPTO_FACTORY_H
 #define CYNG_CRYPTO_FACTORY_H
 
+#include <boost/asio.hpp>
 #include <smfsec/crypto.h>
 #include <string>
 
-namespace cyng
-{
-	namespace crypto
-	{
-		/**
-		 * create a bignum structure
-		 */
-		BN_ptr create_bignum();
-		BN_ptr create_bignum_from_hex(std::string);
-		BN_ptr create_bignum_rsa_f4();
+namespace cyng {
+    namespace crypto {
+        /**
+         * create a bignum structure
+         */
+        BN_ptr create_bignum();
+        BN_ptr create_bignum_from_hex(std::string);
+        BN_ptr create_bignum_rsa_f4();
 
 #if OPENSSL_VERSION_NUMBER < 0x10100000L
-		/**
-		 * create a RSA structure
-		 */
-		RSA_ptr create_rsa();
+        /**
+         * create a RSA structure
+         */
+        RSA_ptr create_rsa();
 
-		/**
-		 * create a RSA key with the specified size (typically 1024 or 2048)
-		 */
-		RSA_ptr create_rsa_key(BIGNUM*, int bits);
+        /**
+         * create a RSA key with the specified size (typically 1024 or 2048)
+         */
+        RSA_ptr create_rsa_key(BIGNUM *, int bits);
 
-		/**
-		 * Create RSA key from EVP structure
-		 */
-		RSA_ptr create_rsa_key(EVP_PKEY*);
+        /**
+         * Create RSA key from EVP structure
+         */
+        RSA_ptr create_rsa_key(EVP_PKEY *);
 
-		/**
-		 * provide a X509 structure.
-		 * Represents an x509 certificate in memory.
-		 * A value of 2 stands for version 1.3, a value of 1 encodes
-		 * version 1.2.
-		 *
-		 * @param v version (usually == 2)
-		 */
-		X509_ptr create_x509(long v);
+        /**
+         * provide a X509 structure.
+         * Represents an x509 certificate in memory.
+         * A value of 2 stands for version 1.3, a value of 1 encodes
+         * version 1.2.
+         *
+         * @param v version (usually == 2)
+         */
+        X509_ptr create_x509(long v);
 
-		/**
-		 * Create a X509 from a certificate string and password
-		 */
-		X509_ptr create_x509(const std::string& certstr, const std::string& pw);
+        /**
+         * Create a X509 from a certificate string and password
+         */
+        X509_ptr create_x509(const std::string &certstr, const std::string &pw);
 
-		/**
-		 * create a x509 request structure
-		 * 
-		 * @param v version (mostly 1)
-		 */
-		X509_REQ_ptr create_x509_request(int v);
+        /**
+         * create a x509 request structure
+         *
+         * @param v version (mostly 1)
+         */
+        X509_REQ_ptr create_x509_request(int v);
 
-		/**
-		 * create a key store
-		 */
-		EVP_PKEY_ptr create_evp_pkey();
+        /**
+         * create a key store
+         */
+        EVP_PKEY_ptr create_evp_pkey();
 
-		/**
-		 * decode the public key for a certificate
-		 */
-		EVP_PKEY_ptr create_evp_pkey(X509* x509);
+        /**
+         * decode the public key for a certificate
+         */
+        EVP_PKEY_ptr create_evp_pkey(X509 *x509);
 
-		/**
-		 * Hold a public and optionally a private key.
-		 * Contains no associated curve.
-		 */
-		EC_KEY_ptr create_ec_key();
+        /**
+         * Hold a public and optionally a private key.
+         * Contains no associated curve.
+         */
+        EC_KEY_ptr create_ec_key();
 
-		/**
-		 * PEM_read_bio_EC_PUBKEY
-		 */
-		EC_KEY_ptr create_ec_pub_key(BIO*, std::string pwd);
+        /**
+         * PEM_read_bio_EC_PUBKEY
+         */
+        EC_KEY_ptr create_ec_pub_key(BIO *, std::string pwd);
 
-		/**
-		 * PEM_read_bio_ECPrivateKey
-		 */
-		EC_KEY_ptr create_ec_priv_key(BIO*, std::string pwd);
+        /**
+         * PEM_read_bio_ECPrivateKey
+         */
+        EC_KEY_ptr create_ec_priv_key(BIO *, std::string pwd);
 
 #endif
 
-		/**
-		 * create a signing context
-		 */
-		EVP_MD_CTX_ptr create_evp_ctx();
+        /**
+         * create a signing context
+         */
+        EVP_MD_CTX_ptr create_evp_ctx();
 
-		/**
-		 * create an SSL context
-		 * The supported protocols are SSLv2, SSLv3, TLSv1, TLSv1.1 and TLSv1.2.
-		 */
-		SSL_CTX_ptr create_ssl_ctx();
-		SSL_CTX_ptr create_ssl_ctx_v23();
-		SSL_CTX_ptr create_ssl_ctx_v23_client();
-		SSL_CTX_ptr create_ssl_ctx_v23_server();
-		SSL_CTX_ptr create_ssl_ctx_dtls();
+        /**
+         * create an SSL context
+         * The supported protocols are SSLv2, SSLv3, TLSv1, TLSv1.1 and TLSv1.2.
+         */
+        SSL_CTX_ptr create_ssl_ctx();
+        SSL_CTX_ptr create_ssl_ctx_v23();
+        SSL_CTX_ptr create_ssl_ctx_v23_client();
+        SSL_CTX_ptr create_ssl_ctx_v23_server();
+        SSL_CTX_ptr create_ssl_ctx_dtls();
 
-		SSLptr create_ssl(SSL_CTX*);
+        SSLptr create_ssl(SSL_CTX *);
 
-		/**
-		 * Pointer is initialized with ASN1_TIME_new().
-		 *
-		 * @return pointer to ASN1_TIME
-		 */
-		ASN1_TIME_ptr create_asn1_time();
+        /**
+         * Pointer is initialized with ASN1_TIME_new().
+         *
+         * @return pointer to ASN1_TIME
+         */
+        ASN1_TIME_ptr create_asn1_time();
 
-		/**
-		 * helper function to populate a x509_name structure.
-		 *
-		 * @param subject entry name
-		 * @param txt text/value of this entry
-		 */
-		bool add_entry_by_txt(X509_NAME*, const char* subject, const char* txt);
+        /**
+         * helper function to populate a x509_name structure.
+         *
+         * @param subject entry name
+         * @param txt text/value of this entry
+         */
+        bool add_entry_by_txt(X509_NAME *, const char *subject, const char *txt);
 
-		/**
-		 * ECDSA_SIG is an opaque structure consisting of two BIGNUMs for the r and s value of an
-		 * ECDSA signature (see X9.62 or FIPS 186-2).
-		 */
-		ECDSA_SIG_ptr create_ecdsa_sig();
-	}
-}
+        /**
+         * ECDSA_SIG is an opaque structure consisting of two BIGNUMs for the r and s
+         * value of an ECDSA signature (see X9.62 or FIPS 186-2).
+         */
+        ECDSA_SIG_ptr create_ecdsa_sig();
 
-#endif	
+        /**
+         * There gazillions of BIO methods
+         */
+        BIO_ptr create_bio(BIO_METHOD const *);
+        BIO_ptr create_bio_s_mem();
+        BIO_ptr create_bio_s_secmem();
+#ifndef OPENSSL_NO_SOCK
+        BIO_ptr create_bio_s_socket();
+        BIO_ptr create_bio_s_connect();
+        BIO_ptr create_bio_s_accept();
+#endif
+        BIO_ptr create_bio_s_fd();
+        BIO_ptr create_bio_s_log();
+        BIO_ptr create_bio_s_bio();
+        BIO_ptr create_bio_s_null();
+        BIO_ptr create_bio_f_null();
+        BIO_ptr create_bio_f_buffer();
+        BIO_ptr create_bio_f_linebuffer();
+        BIO_ptr create_bio_f_nbio_test();
 
+        BIO_ptr create_connection();
+    } // namespace crypto
+} // namespace cyng
+
+#endif
--- src/factory.cpp
+++ src/factory.cpp
@@ -1,168 +1,133 @@
 /*
-* The MIT License (MIT)
-*
-* Copyright (c) 2019 Sylko Olzscher
-*
-*/
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Sylko Olzscher
+ *
+ */
 
-#include <smfsec/factory.h>
-#include <smfsec/bio.h>
 #include <assert.h>
+#include <smfsec/bio.h>
+#include <smfsec/factory.h>
 
-namespace cyng 
-{
-	namespace crypto
-	{
-		BN_ptr create_bignum()
-		{
-			return BN_ptr(BN_new(), BN_free);
-		}
+namespace cyng {
+    namespace crypto {
+        BN_ptr create_bignum() { return BN_ptr(BN_new(), BN_free); }
 
-		BN_ptr create_bignum_from_hex(std::string s) {
-			auto p = create_bignum();
-			BIGNUM * bn = p.get();
-			auto const r = BN_hex2bn(&bn, s.c_str());
-			return p;
-		}
+        BN_ptr create_bignum_from_hex(std::string s) {
+            auto p = create_bignum();
+            BIGNUM *bn = p.get();
+            auto const r = BN_hex2bn(&bn, s.c_str());
+            return p;
+        }
 
-		BN_ptr create_bignum_rsa_f4()
-		{
-			auto p = create_bignum();
-			auto ret = BN_set_word(p.get(), RSA_F4);
-			assert(ret == 1);
-			return p;
-		}
+        BN_ptr create_bignum_rsa_f4() {
+            auto p = create_bignum();
+            auto ret = BN_set_word(p.get(), RSA_F4);
+            assert(ret == 1);
+            return p;
+        }
 
 #if OPENSSL_VERSION_NUMBER < 0x10100000L
 
-		RSA_ptr create_rsa()
-		{
-			return RSA_ptr(::RSA_new(), ::RSA_free);
-		}
+        RSA_ptr create_rsa() { return RSA_ptr(::RSA_new(), ::RSA_free); }
 
-		RSA_ptr create_rsa_key(BIGNUM* bnp, int bits)
-		{
-			auto p = create_rsa();
-			auto ret = RSA_generate_key_ex(p.get(), bits, bnp, NULL);
-			assert(ret == 1);
-			return p;
-		}
+        RSA_ptr create_rsa_key(BIGNUM *bnp, int bits) {
+            auto p = create_rsa();
+            auto ret = RSA_generate_key_ex(p.get(), bits, bnp, NULL);
+            assert(ret == 1);
+            return p;
+        }
 
-		RSA_ptr create_rsa_key(EVP_PKEY* key)
-		{
-			return (key != nullptr)
-				? RSA_ptr(::EVP_PKEY_get1_RSA(key), ::RSA_free)
-				: create_rsa()
-				;
-		}
+        RSA_ptr create_rsa_key(EVP_PKEY *key) {
+            return (key != nullptr) ? RSA_ptr(::EVP_PKEY_get1_RSA(key), ::RSA_free) : create_rsa();
+        }
 
+        X509_ptr create_x509(long v) {
+            auto p = X509_ptr(X509_new(), X509_free);
+            X509_set_version(p.get(), v);
+            return p;
+        }
 
-		X509_ptr create_x509(long v)
-		{
-			auto p = X509_ptr(X509_new(), X509_free);
-			X509_set_version(p.get(), v);
-			return p;
-		}
+        X509_ptr create_x509(const std::string &certstr, const std::string &pw) {
+            auto certbio = create_bio_str(certstr);
+            X509 *x509 = PEM_read_bio_X509(certbio.get(), nullptr, nullptr, const_cast<char *>(pw.c_str()));
+            return X509_ptr(x509, X509_free);
+        }
 
-		X509_ptr create_x509(const std::string& certstr, const std::string& pw)
-		{
-			auto certbio = create_bio_str(certstr);
-			X509* x509 = PEM_read_bio_X509(certbio.get(), nullptr, nullptr, const_cast<char*>(pw.c_str()));
-			return X509_ptr(x509, X509_free);
-		}
+        X509_REQ_ptr create_x509_request(int v) {
+            auto p = X509_REQ_ptr(X509_REQ_new(), X509_REQ_free);
+            auto ret = X509_REQ_set_version(p.get(), v);
+            assert(ret == 1);
+            return p;
+        }
 
-		X509_REQ_ptr create_x509_request(int v)
-		{
-			auto p = X509_REQ_ptr(X509_REQ_new(), X509_REQ_free);
-			auto ret = X509_REQ_set_version(p.get(), v);
-			assert(ret == 1);
-			return p;
-		}
+        EVP_PKEY_ptr create_evp_pkey() { return EVP_PKEY_ptr(EVP_PKEY_new(), EVP_PKEY_free); }
 
-		EVP_PKEY_ptr create_evp_pkey()
-		{
-			return EVP_PKEY_ptr(EVP_PKEY_new(), EVP_PKEY_free);
-		}
+        EVP_PKEY_ptr create_evp_pkey(X509 *x509) {
+            assert(x509 != nullptr);
+            return EVP_PKEY_ptr(X509_get_pubkey(x509), EVP_PKEY_free);
+        }
 
-		EVP_PKEY_ptr create_evp_pkey(X509* x509)
-		{
-			assert(x509 != nullptr);
-			return EVP_PKEY_ptr(X509_get_pubkey(x509), EVP_PKEY_free);
-		}
+        EC_KEY_ptr create_ec_key() { return EC_KEY_ptr(EC_KEY_new(), EC_KEY_free); }
 
-		EC_KEY_ptr create_ec_key()
-		{
-			return EC_KEY_ptr(EC_KEY_new(), EC_KEY_free);
-		}
+        EC_KEY_ptr create_ec_pub_key(BIO *bio, std::string pub_pwd) {
+            return EC_KEY_ptr(::PEM_read_bio_EC_PUBKEY(bio, nullptr, nullptr, const_cast<char *>(pub_pwd.c_str())), ::EC_KEY_free);
+        }
 
-		EC_KEY_ptr create_ec_pub_key(BIO* bio, std::string pub_pwd)
-		{
-			return EC_KEY_ptr(::PEM_read_bio_EC_PUBKEY(bio, nullptr, nullptr, const_cast<char*>(pub_pwd.c_str())), ::EC_KEY_free);
-		}
-
-		EC_KEY_ptr create_ec_priv_key(BIO* bio, std::string priv_pwd)
-		{
-			return EC_KEY_ptr(::PEM_read_bio_ECPrivateKey(bio, nullptr, nullptr, const_cast<char*>(priv_pwd.c_str())), ::EC_KEY_free);
-		}
+        EC_KEY_ptr create_ec_priv_key(BIO *bio, std::string priv_pwd) {
+            return EC_KEY_ptr(
+                ::PEM_read_bio_ECPrivateKey(bio, nullptr, nullptr, const_cast<char *>(priv_pwd.c_str())), ::EC_KEY_free);
+        }
 
 #endif
 
-		EVP_MD_CTX_ptr create_evp_ctx()
-		{
-			//If openssl version less than 1.1
+        EVP_MD_CTX_ptr create_evp_ctx() {
+            // If openssl version less than 1.1
 #if OPENSSL_VERSION_NUMBER < 0x10100000L
-			return EVP_MD_CTX_ptr(EVP_MD_CTX_create(), EVP_MD_CTX_destroy);
+            return EVP_MD_CTX_ptr(EVP_MD_CTX_create(), EVP_MD_CTX_destroy);
 #else
-			return EVP_MD_CTX_ptr(EVP_MD_CTX_new(), EVP_MD_CTX_free);
+            return EVP_MD_CTX_ptr(EVP_MD_CTX_new(), EVP_MD_CTX_free);
 #endif
-		}
+        }
 
-		SSL_CTX_ptr create_ssl_ctx()
-		{
-			return SSL_CTX_ptr(nullptr, SSL_CTX_free);
-		}
+        SSL_CTX_ptr create_ssl_ctx() { return SSL_CTX_ptr(nullptr, SSL_CTX_free); }
 
-		SSL_CTX_ptr create_ssl_ctx_v23()
-		{
-			return SSL_CTX_ptr(SSL_CTX_new(SSLv23_method()), SSL_CTX_free);
-		}
+        SSL_CTX_ptr create_ssl_ctx_v23() { return SSL_CTX_ptr(SSL_CTX_new(SSLv23_method()), SSL_CTX_free); }
 
-		SSL_CTX_ptr create_ssl_ctx_v23_client()
-		{
-			return SSL_CTX_ptr(SSL_CTX_new(SSLv23_client_method()), SSL_CTX_free);
-		}
+        SSL_CTX_ptr create_ssl_ctx_v23_client() { return SSL_CTX_ptr(SSL_CTX_new(SSLv23_client_method()), SSL_CTX_free); }
 
-		SSL_CTX_ptr create_ssl_ctx_v23_server()
-		{
-			return SSL_CTX_ptr(SSL_CTX_new(SSLv23_server_method()), SSL_CTX_free);
-		}
+        SSL_CTX_ptr create_ssl_ctx_v23_server() { return SSL_CTX_ptr(SSL_CTX_new(SSLv23_server_method()), SSL_CTX_free); }
 
-		SSL_CTX_ptr create_ssl_ctx_dtls()
-		{
-			return SSL_CTX_ptr(SSL_CTX_new(DTLS_method()), SSL_CTX_free);
-		}
+        SSL_CTX_ptr create_ssl_ctx_dtls() { return SSL_CTX_ptr(SSL_CTX_new(DTLS_method()), SSL_CTX_free); }
 
+        SSLptr create_ssl(SSL_CTX *ctx) { return SSLptr(SSL_new(ctx), SSL_free); }
 
-		SSLptr create_ssl(SSL_CTX* ctx)
-		{
-			return SSLptr(SSL_new(ctx), SSL_free);
-		}
+        ASN1_TIME_ptr create_asn1_time() { return ASN1_TIME_ptr(ASN1_TIME_new(), ASN1_STRING_free); }
 
-		ASN1_TIME_ptr create_asn1_time()
-		{
-			return ASN1_TIME_ptr(ASN1_TIME_new(), ASN1_STRING_free);
-		}
+        bool add_entry_by_txt(X509_NAME *x509_name, const char *subject, const char *txt) {
+            auto const ret = X509_NAME_add_entry_by_txt(x509_name, subject, MBSTRING_ASC, (const unsigned char *)txt, -1, -1, 0);
+            return ret == 1;
+        }
 
-		bool add_entry_by_txt(X509_NAME* x509_name, const char* subject, const char* txt)
-		{
-			auto const ret = X509_NAME_add_entry_by_txt(x509_name, subject, MBSTRING_ASC, (const unsigned char*)txt, -1, -1, 0);
-			return ret == 1;
-		}
+        ECDSA_SIG_ptr create_ecdsa_sig() { return ECDSA_SIG_ptr(::ECDSA_SIG_new(), ::ECDSA_SIG_free); }
 
-		ECDSA_SIG_ptr create_ecdsa_sig()
-		{
-			return ECDSA_SIG_ptr(::ECDSA_SIG_new(), ::ECDSA_SIG_free);
-		}
+        BIO_ptr create_bio(BIO_METHOD const *pm) { return BIO_ptr(BIO_new(pm), BIO_free); }
 
-	}
-}
+        BIO_ptr create_bio_s_mem() { return create_bio(BIO_s_mem()); }
+        BIO_ptr create_bio_s_secmem() { return create_bio(BIO_s_secmem()); }
+#ifndef OPENSSL_NO_SOCK
+        BIO_ptr create_bio_s_socket() { return create_bio(BIO_s_socket()); }
+        BIO_ptr create_bio_s_connect() { return create_bio(BIO_s_connect()); }
+        BIO_ptr create_bio_s_accept() { return create_bio(BIO_s_accept()); }
+#endif
+        BIO_ptr create_bio_s_fd() { return create_bio(BIO_s_fd()); }
+        BIO_ptr create_bio_s_log() { return create_bio(BIO_s_log()); }
+        BIO_ptr create_bio_s_bio() { return create_bio(BIO_s_bio()); }
+        BIO_ptr create_bio_s_null() { return create_bio(BIO_s_null()); }
+        BIO_ptr create_bio_f_null() { return create_bio(BIO_f_null()); }
+        BIO_ptr create_bio_f_buffer() { return create_bio(BIO_f_buffer()); }
+        BIO_ptr create_bio_f_linebuffer() { return create_bio(BIO_f_linebuffer()); }
+        BIO_ptr create_bio_f_nbio_test() { return create_bio(BIO_f_nbio_test()); }
+
+    } // namespace crypto
+} // namespace cyng
